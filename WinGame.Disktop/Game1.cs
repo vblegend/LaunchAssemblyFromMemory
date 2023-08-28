19018edf0f12c48a8b0b132ac27f71f77a97a56f
Hanks <vblegend@qq.com>
2023-08-29 00:19:55 +0800

    发过火
    法国恢复供货

--- WinGame.Disktop/Game1.cs
+++ WinGame.Disktop/Game1.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.BitmapFonts;
+using MonoGame.Extended.Gui;
+using MonoGame.Extended.Gui.Controls;
+using MonoGame.Extended.ViewportAdapters;
 using System;
+ 
 
 namespace WinGame.Disktop
 {
@@ -12,12 +18,13 @@ namespace WinGame.Disktop
 
     public class Game1 : Game
     {
+        private GuiSystem _guiSystem;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D _iconTexture;
         private VertexPositionColor[] _vertexPositionColors;
         private BasicEffect _basicEffect;
-
+        private Texture2D _textTexture;
 
         public Game1()
         {
@@ -55,7 +62,115 @@ namespace WinGame.Disktop
             _basicEffect.VertexColorEnabled = true;
             _basicEffect.World = Matrix.CreateOrthographicOffCenter(0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0, 0, 1);
 
+            var font = Content.Load<BitmapFont>("Sensation");
+            BitmapFont.UseKernings = false;
+            Skin.CreateDefault(font);
+
+
+ 
+
+
+            var stackTest = new DemoViewModel("Stack Panels",
+                    new StackPanel
+                    {
+                        Items =
+                        {
+                            new Button { Content = "Press Me", HorizontalAlignment = HorizontalAlignment.Left, VerticalAlignment = VerticalAlignment.Top },
+                            new Button { Content = "Press Me", HorizontalAlignment = HorizontalAlignment.Right, VerticalAlignment = VerticalAlignment.Bottom  },
+                            new Button { Content = "Press Me", HorizontalAlignment = HorizontalAlignment.Centre, VerticalAlignment = VerticalAlignment.Centre  },
+                            new Button { Content = "Press Me", HorizontalAlignment = HorizontalAlignment.Stretch, VerticalAlignment = VerticalAlignment.Stretch },
+                        }
+                    });
+
+            var dockTest = new DemoViewModel("Dock Panels",
+                new DockPanel
+                {
+                    Items =
+                    {
+                        new Button { Content = "Dock.Top", AttachedProperties = { { DockPanel.DockProperty, Dock.Top } } },
+                        new Button { Content = "Dock.Bottom", AttachedProperties = { { DockPanel.DockProperty, Dock.Bottom } } },
+                        new Button { Content = "Dock.Left", AttachedProperties = { { DockPanel.DockProperty, Dock.Left } } },
+                        new Button { Content = "Dock.Right", AttachedProperties = { { DockPanel.DockProperty, Dock.Right } } },
+                        new Button { Content = "Fill" }
+                    }
+                });
+
+            var controlTest = new DemoViewModel("Basic Controls",
+                new StackPanel
+                {
+                    Margin = 5,
+                    Orientation = Orientation.Vertical,
+                    Items =
+                    {
+                        new Label("Buttons") { Margin = 5 },
+                        new StackPanel
+                        {
+                            Orientation = Orientation.Horizontal,
+                            Spacing = 5,
+                            Items =
+                            {
+                                new Button { Content = "Enabled" },
+                                new Button { Content = "Disabled", IsEnabled = false },
+                                new ToggleButton { Content = "ToggleButton" }
+                            }
+                        },
+
+                        new Label("TextBox") { Margin = 5 },
+                        new TextBox {Text = "TextBox" },
+
+                        new Label("CheckBox") { Margin = 5 },
+                        new CheckBox {Content = "Check me please!"},
+
+                        new Label("ListBox") { Margin = 5 },
+                        new ListBox {Items = {"ListBoxItem1", "ListBoxItem2", "ListBoxItem3"}, SelectedIndex = 0},
+
+                        new Label("ProgressBar") { Margin = 5 },
+                        new ProgressBar {Progress = 0.5f, Width = 100},
+
+                        new Label("ComboBox") { Margin = 5 },
+                        new ComboBox {Items = {"ComboBoxItemA", "ComboBoxItemB", "ComboBoxItemC"}, SelectedIndex = 0, HorizontalAlignment = HorizontalAlignment.Left}
+                    }
+                });
+
+            var demoScreen = new Screen
+            {
+                Content = new DockPanel
+                {
+                    LastChildFill = true,
+                    Items =
+                    {
+                        new ListBox
+                        {
+                            Name = "DemoList",
+                            AttachedProperties = { { DockPanel.DockProperty, Dock.Left} },
+                            ItemPadding = new Thickness(5),
+                            VerticalAlignment = VerticalAlignment.Stretch,
+                            HorizontalAlignment = HorizontalAlignment.Left,
+                            SelectedIndex = 0,
+                            Items = { controlTest, stackTest, dockTest }
+                        },
+                        new ContentControl
+                        {
+                            Name = "Content",
+                            BackgroundColor = new Color(30, 30, 30)
+                        }
+                    }
+                }
+            };
+            var viewportAdapter = new DefaultViewportAdapter(GraphicsDevice);
+            var guiRenderer = new GuiSpriteBatchRenderer(GraphicsDevice, () => Matrix.Identity);
+            _guiSystem = new GuiSystem(viewportAdapter, guiRenderer) { ActiveScreen = demoScreen };
+
+            var demoList = demoScreen.FindControl<ListBox>("DemoList");
+            var demoContent = demoScreen.FindControl<ContentControl>("Content");
+
+            demoList.SelectedIndexChanged += (sender, args) => demoContent.Content = (demoList.SelectedItem as DemoViewModel)?.Content;
+            demoContent.Content = (demoList.SelectedItem as DemoViewModel)?.Content;
+
+
+            _textTexture = DrawString("Hello，你好啊！@#￥%%……&", 0, 0, Color.Red, new System.Drawing.Font("Consolas", 14, System.Drawing.FontStyle.Bold));
         }
+
 
         protected override void Update(GameTime gameTime)
         {
@@ -65,6 +180,11 @@ namespace WinGame.Disktop
             // TODO: Add your update logic here
             value -= 1;
             if (value < 0) value = 360;
+
+
+            _guiSystem.Update(gameTime);
+
+
             base.Update(gameTime);
         }
 
@@ -79,14 +199,20 @@ namespace WinGame.Disktop
             GraphicsDevice.Clear(Microsoft.Xna.Framework.Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            this.DrawMarkRect(new Rectangle(200, 200, 100, 100), value, new Color(255, 0, 0, 80));
+            this.Fill(new Rectangle(100, 300, 100, 100), new Color(255, 0, 0, 80));
+            //_guiSystem.Draw(gameTime);
             _spriteBatch.Begin();
             _spriteBatch.Draw(this._iconTexture, new Rectangle(0, 0, 64, 64), Color.White);
             Primitives2D.DrawLine(_spriteBatch, new Vector2(100, 100), new Vector2(200, 300), Color.Red, 5.0f);
+
+            
+            _spriteBatch.Draw(_textTexture, new Rectangle(0, 0, _textTexture.Width, _textTexture.Height), Color.Yellow); //...and draw it!
+
+
             _spriteBatch.End();
 
-
-
+ 
 
             //GraphicsDevice.RasterizerState = RasterizerState.CullNone;
             //GraphicsDevice.RasterizerState = new RasterizerState
@@ -110,15 +236,69 @@ namespace WinGame.Disktop
             //    GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, _vertexPositionColors, 0, 2);
             //}
 
-            this.DrawMarkRect(new Rectangle(200, 200, 100, 100), value, new Color(255,0,0,80));
 
 
-   
 
 
-            this.Fill(new Rectangle(100, 300, 100, 100), new Color(255, 0, 0, 80));
             base.Draw(gameTime);
         }
+
+
+
+
+
+
+        public Vector2 MeasureString(string text, System.Drawing.Font font, int wrapWidth = int.MaxValue)
+        {
+            using (var gfx = System.Drawing.Graphics.FromImage(new System.Drawing.Bitmap(1, 1)))
+            {
+                var s = gfx.MeasureString(text, font, wrapWidth); //SmartMeasureString is an extension method I made for System.Drawing.Graphics which applies text rendering hints and formatting rules that I need to make text rendering and measurement accurate and usable without copy-pasting the same code.
+                return new Vector2((float)Math.Ceiling(s.Width), (float)Math.Ceiling(s.Height)); //Better to round up the values returned by SmartMeasureString - it's just easier math-wise to deal with whole numbers
+            }
+        }
+
+        public Texture2D DrawString(string text, int x, int y, Color color, System.Drawing.Font font, int wrapWidth = 0)
+        {
+            //_startx and _starty are used for making sure coordinates are relative to the clip bounds of the current context
+            Vector2 measure;
+            if (wrapWidth == 0)
+                measure = MeasureString(text, font);
+            else
+                measure = MeasureString(text, font, wrapWidth);
+            using (var bmp = new System.Drawing.Bitmap((int)measure.X, (int)measure.Y))
+            {
+                using (var gfx = System.Drawing.Graphics.FromImage(bmp))
+                {
+                    //gfx.Clear(System.Drawing.Color.Red);
+                    var textformat = new System.Drawing.StringFormat(System.Drawing.StringFormat.GenericTypographic);
+                    textformat.FormatFlags = System.Drawing.StringFormatFlags.MeasureTrailingSpaces;
+                    textformat.Trimming = System.Drawing.StringTrimming.None;
+                    textformat.FormatFlags |= System.Drawing.StringFormatFlags.NoClip; //without this, text gets cut off near the right edge of the string bounds
+
+                    gfx.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixel; //Anything but this and performance takes a dive.
+                    gfx.DrawString(text, font, new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B)), 0, 0, textformat);
+                }
+
+                var lck = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb); //Lock the bitmap in memory and give us the ability to extract data from it so we can load it into a Texture2D
+                var data = new byte[Math.Abs(lck.Stride) * lck.Height]; //destination array for bitmap data, source for texture data
+                System.Runtime.InteropServices.Marshal.Copy(lck.Scan0, data, 0, data.Length); //cool, data's in the destination array
+                bmp.UnlockBits(lck); //Unlock the bits. We don't need 'em.
+                var tex2 = new Texture2D(GraphicsDevice, bmp.Width, bmp.Height);
+                
+                    for (int i = 0; i < data.Length; i += 4)
+                    {
+                        byte r = data[i];
+                        byte b = data[i + 2];
+                        data[i] = b;
+                        data[i + 2] = r;
+                    } //This code swaps the red and blue values of each pixel in the bitmap so that they are arranged as BGRA. If we don't do this, we get weird rendering glitches where red text is blue etc.
+
+                    tex2.SetData<byte>(data); //Load the data into the texture
+                    //_spriteBatch.Draw(tex2, new Rectangle(x, y, bmp.Width, bmp.Height), Color.Red); //...and draw it!
+                return tex2;
+            }
+        }
+
 
 
 
@@ -228,6 +408,26 @@ namespace WinGame.Disktop
 
 
     }
+
+
+
+    public class DemoViewModel
+    {
+        public DemoViewModel(string name, object content)
+        {
+            Name = name;
+            Content = content;
+        }
+
+        public string Name { get; }
+        public object Content { get; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
 }
 
 
