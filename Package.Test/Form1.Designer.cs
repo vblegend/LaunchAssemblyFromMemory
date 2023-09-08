d6754db09725b23c5b93a158e4fccce03f7ff213
Hanks <vblegend@qq.com>
2023-09-08 22:29:52 +0800

    111

---
+++ Package.Test/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Package.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            opend = new Button();
+            SuspendLayout();
+            // 
+            // opend
+            // 
+            opend.Location = new Point(48, 54);
+            opend.Name = "opend";
+            opend.Size = new Size(144, 40);
+            opend.TabIndex = 0;
+            opend.Text = "Open";
+            opend.UseVisualStyleBackColor = true;
+            opend.Click += opend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(opend);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button opend;
+    }
+}
